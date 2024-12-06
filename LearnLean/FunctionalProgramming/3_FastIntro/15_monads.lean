466c13dd4811d2c5d30bdb0e65bdeef16214b83b
Davi Sales Barreira <davisbarreira@gmail.com>
2024-12-06 17:23:06 -0300

    :notebook: my monad implementation.;

--- LearnLean/FunctionalProgramming/3_FastIntro/15_monads.lean
+++ LearnLean/FunctionalProgramming/3_FastIntro/15_monads.lean
@@ -2,6 +2,11 @@ namespace Monad
 
 -- This is my Personal Implementation for a Monad
 -- Monad is a monoid in the category of endofunctors
+-- Thus, it is characterized by an η (pure) and a μ : M M → M operator,
+-- which is analogous to the monoidal operator ⊕ : M × M → M.
+-- In Functional Programming, it is more
+-- Common to define a Monad using an operator called `bind : M α → (α → M β) → M β`
+-- The bind can be used to derive the μ and vice-versa
 class Monad (m : Type → Type) [Functor m] where
   η {α : Type} :  α → m α
   μ {α} : m (m α) → m α
@@ -14,10 +19,11 @@ instance : Monad Option where
     | some a => a
     | none => none
 
-def mbind {α β : Type} {m : Type → Type} [Functor m] [Monad m] : m α → (α → m β) → m β :=
+-- Let us define the `bind` operator.
+def Monad.bind {α β : Type} {m : Type → Type} [Functor m] [Monad m] : m α → (α → m β) → m β :=
   fun ma f => Monad.μ (Functor.map f ma)
 
-infixl:55 " >=> " => mbind
+infixl:55 " >=> " => Monad.bind
 
 #eval Monad.μ (some (some 10))
 
@@ -26,6 +32,8 @@ def myf (a : Nat) : Option Nat :=
   else none
 
 #eval Monad.μ (Functor.map myf (some 20))
+#eval some 20 >=> myf
+#eval (none : Option Nat) >=> myf
 
 #eval (fun x => x+1) <$> some 10
 
